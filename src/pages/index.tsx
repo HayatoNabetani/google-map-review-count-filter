ca1efc45f95036ef860aec2ad24261a04bdef9e3
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2023-03-07 12:41:33 +0900

    UIを整えるまで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,8 @@ import {
 } from "@react-google-maps/api";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { MAP_API_KEY } from "../constant/env";
+import { MapPinIcon, CalculatorIcon } from "@heroicons/react/24/solid";
+import { Rating } from "react-simple-star-rating";
 
 const containerStyle = {
     height: "100vh",
@@ -59,17 +61,22 @@ const Home: NextPage = () => {
 
     const [places, setPlaces] = useState<any>([]);
     const [searchValue, setSearchValue] = useState("");
+    const [nowPosition, setNowPosition] = useState<any>({});
+    const [rating, setRating] = useState(0);
+    const handleRating = (rate: number) => {
+        setRating(rate);
+    };
+    const onPointerEnter = () => console.log("Enter");
+    const onPointerLeave = () => console.log("Leave");
+    const onPointerMove = (value: number, index: number) =>
+        console.log(value, index);
 
     const handleSearch = () => {
         console.log("test");
         const service = new window.google.maps.places.PlacesService(map!);
-        const center = {
-            lat: 35.69731,
-            lng: 139.7747,
-        };
         const request: any = {
-            location: center,
-            radius: 5000,
+            location: nowPosition,
+            radius: 1000,
             // type: ["restaurant"],
             keyword: searchValue,
         };
@@ -84,15 +91,33 @@ const Home: NextPage = () => {
     };
 
     const onLoad = useCallback((map: Map) => {
-        const bounds = new window.google.maps.LatLngBounds(
-            clickedLatLng,
-            clickedLatLng2
-        );
-        // 指定した境界がちょうどよく見えるように、地図のビューポート(位置座標とズーム値)を変更してくれます。
-        map.fitBounds(bounds);
-        // const pl = new window.google.maps.places;
-        // console.log(v);
-        setMap(map);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    const positionData = position.coords;
+                    const _nowPosition = {
+                        lat: positionData.latitude,
+                        lng: positionData.longitude,
+                    };
+                    const bounds = new window.google.maps.LatLngBounds(
+                        _nowPosition
+                    );
+                    setNowPosition(_nowPosition);
+                    map.fitBounds(bounds);
+                    setMap(map);
+                },
+                function (error: any) {
+                    console.log(error);
+                },
+                {
+                    enableHighAccuracy: false,
+                    timeout: 8000,
+                    maximumAge: 2000,
+                }
+            );
+        } else {
+            setMap(map);
+        }
     }, []);
 
     useEffect(() => {
@@ -106,7 +131,7 @@ const Home: NextPage = () => {
                 bounds.extend(position);
                 return place.id;
             });
-            bounds.extend(clickedLatLng);
+            // bounds.extend(clickedLatLng);
             map.fitBounds(bounds);
         }
     }, [map, places]);
@@ -131,55 +156,177 @@ const Home: NextPage = () => {
             <h1 className="text-3xl font-bold underline text-center">
                 Google Map 口コミ数フィルター
             </h1>
-            <div>
-                検索
-                <input
-                    className="border"
-                    type="text"
-                    placeholder="a"
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                />
-                <button type="button" onClick={handleSearch}>
-                    検索！
-                </button>
-            </div>
             <>
-                {isLoaded ? (
-                    <GoogleMap
-                        mapContainerStyle={containerStyle}
-                        onLoad={onLoad}
-                        zoom={20}
-                        // center={clickedLatLng}
-                        onClick={handleClick}
-                    >
-                        <MarkerF
-                            position={clickedLatLng2}
-                            label={markerLabel}
-                        />
-                        {places.map((place: any, i: number) => {
-                            const position = {
-                                lat: place.geometry.location.lat(),
-                                lng: place.geometry.location.lng(),
-                            };
-                            const label: google.maps.MarkerLabel = {
-                                text: place.name,
-                                fontFamily: "sans-serif",
-                                fontSize: "10px",
-                                fontWeight: "bold",
-                            };
-                            return (
-                                <MarkerF
-                                    key={place.name}
-                                    position={position}
-                                    label={label}
+                <div className="flex">
+                    <div className="flex flex-col h-full p-3 w-60">
+                        <div className="space-y-3">
+                            <div className="flex items-center justify-between">
+                                <h2>フィルター</h2>
+                                <button className="p-2">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 512 512"
+                                        className="w-5 h-5 fill-current"
+                                    >
+                                        <rect
+                                            width="352"
+                                            height="32"
+                                            x="80"
+                                            y="96"
+                                        ></rect>
+                                        <rect
+                                            width="352"
+                                            height="32"
+                                            x="80"
+                                            y="240"
+                                        ></rect>
+                                        <rect
+                                            width="352"
+                                            height="32"
+                                            x="80"
+                                            y="384"
+                                        ></rect>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="relative">
+                                <span className="absolute inset-y-0 left-0 flex items-center py-4">
+                                    <button
+                                        type="submit"
+                                        className="p-2 focus:outline-none focus:ring"
+                                    >
+                                        <MapPinIcon className="w-5 h-5" />
+                                    </button>
+                                </span>
+                                <input
+                                    type="search"
+                                    name="Search"
+                                    defaultValue={"現在地"}
+                                    placeholder="現在地 or 検索 or 指定"
+                                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none border"
                                 />
-                            );
-                        })}
-                    </GoogleMap>
-                ) : (
-                    "loading"
-                )}
+                            </div>
+                            <div className="relative">
+                                <span className="absolute inset-y-0 left-0 flex items-center py-4">
+                                    <button
+                                        type="submit"
+                                        className="p-2 focus:outline-none focus:ring"
+                                    >
+                                        <svg
+                                            fill="currentColor"
+                                            viewBox="0 0 512 512"
+                                            className="w-5 h-5 dark:text-gray-400"
+                                        >
+                                            <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
+                                        </svg>
+                                    </button>
+                                </span>
+                                <input
+                                    type="search"
+                                    name="Search"
+                                    placeholder="キーワード"
+                                    value={searchValue}
+                                    onChange={(e) =>
+                                        setSearchValue(e.target.value)
+                                    }
+                                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none border"
+                                />
+                            </div>
+                            <div className="relative">
+                                <label
+                                    htmlFor="rounded"
+                                    className="block mb-2 text-sm font-medium"
+                                >
+                                    半径(km以内)
+                                </label>
+                                <input
+                                    id="rounded"
+                                    type="range"
+                                    min="0"
+                                    max="100"
+                                    step="5"
+                                    className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+                                />
+                            </div>
+                            <div>
+                                <Rating
+                                    // size={25}
+                                    emptyStyle={{ display: "flex" }}
+                                    fillStyle={{
+                                        display: "-webkit-inline-box",
+                                    }}
+                                    transition
+                                    onClick={handleRating}
+                                    onPointerEnter={onPointerEnter}
+                                    onPointerLeave={onPointerLeave}
+                                    onPointerMove={onPointerMove}
+                                />
+                            </div>
+                            <div className="relative">
+                                <span className="absolute inset-y-0 left-0 flex items-center py-4">
+                                    <button
+                                        type="submit"
+                                        className="p-2 focus:outline-none focus:ring"
+                                    >
+                                        <CalculatorIcon className="w-5 h-5" />
+                                    </button>
+                                </span>
+                                <input
+                                    id="reviews"
+                                    type="number"
+                                    placeholder="レビュー数"
+                                    step="5"
+                                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none border"
+                                />
+                            </div>
+                            <div>
+                                <button
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                    onClick={handleSearch}
+                                >
+                                    検索！
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-5 flex-auto">
+                        {isLoaded ? (
+                            <GoogleMap
+                                mapContainerStyle={containerStyle}
+                                onLoad={onLoad}
+                                zoom={17}
+                                // center={clickedLatLng}
+                                onClick={handleClick}
+                            >
+                                <MarkerF
+                                    position={nowPosition}
+                                    label={markerLabel}
+                                />
+                                {places.map((place: any, i: number) => {
+                                    const position = {
+                                        lat: place.geometry.location.lat(),
+                                        lng: place.geometry.location.lng(),
+                                    };
+                                    const label: google.maps.MarkerLabel = {
+                                        text: place.name,
+                                        fontFamily: "sans-serif",
+                                        fontSize: "10px",
+                                        fontWeight: "bold",
+                                    };
+                                    return (
+                                        <MarkerF
+                                            key={place.name}
+                                            position={position}
+                                            label={label}
+                                        />
+                                    );
+                                })}
+                            </GoogleMap>
+                        ) : (
+                            "loading"
+                        )}
+                    </div>
+                </div>
             </>
         </div>
     );
